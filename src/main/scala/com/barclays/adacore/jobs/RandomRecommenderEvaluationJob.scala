64e01a033a1f0789f79ffde288e2e956b0703647
Spacagna, Gianmario : Personal & Corporate Banking <Gianmario.Spacagna@barclayscorp.com>
2015-12-08 19:23:49 +0000

    changed api for recommender

--- src/main/scala/com/barclays/adacore/jobs/RandomRecommenderEvaluationJob.scala
+++ src/main/scala/com/barclays/adacore/jobs/RandomRecommenderEvaluationJob.scala
@@ -5,17 +5,18 @@ import com.barclays.adacore.utils.Logger
 import org.apache.spark.{SparkConf, SparkContext}
 import org.rogach.scallop.ScallopConf
 
-case object RandomRecommenderJob {
+case object RandomRecommenderEvaluationJob {
   def main(args: Array[String]) {
     val conf = new ScallopConf(args) {
       val anonymizedDataPath = opt[String](required = true, descr = "The path of the anonymized data")
       val tmpFolder = opt[String](descr = "Overrides the directory used in spark.local.dir")
       val n = opt[Int](default = Some(20), descr = "the number of recommendation to evaluate")
-      val randomRecommendations = opt[Int](default = Some(40), descr = "the number of random recommendation to generate")
       val partitions = opt[Int](default = Some(8), descr = "the number of partitions to coalesce to")
       val persist = toggle(default = Some(false), descrYes = "persist data to disk and memory serialized")
       val trainingFraction = opt[Double](default = Some(0.8),
         descr = "the fraction of businesses to use in the training for each customer")
+
+      val evaluationSamplingFraction = opt[Double](default = Some(1.0))
     }
 
     val sparkConf =
@@ -27,10 +28,12 @@ case object RandomRecommenderJob {
     Logger().info(conf.summary)
 
     val data = sc.textFile(conf.anonymizedDataPath()).coalesce(conf.partitions()).map(AnonymizedRecord.fromSv())
-    val recommender = RandomRecommender(sc, conf.randomRecommendations())
+    val recommender = RandomRecommender(sc)
 
     val (trainingData, testData) = RecommenderEvaluation(sc).splitData(data, conf.trainingFraction())
 
-    Logger().info("MAP@" + conf.n() + "=" + RecommenderEvaluation(sc).evaluate(recommender, trainingData, testData, conf.n()))
+    Logger().info("MAP@" + conf.n() + "=" +
+      RecommenderEvaluation(sc).evaluate(recommender, trainingData, testData, conf.n(), conf.evaluationSamplingFraction())
+    )
   }
 }
