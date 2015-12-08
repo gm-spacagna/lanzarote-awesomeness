c30a250ff6dfd6da512666b8102bbfce9cddd4cb
Victor Paraschiv <victorparaschiv@Victors-MacBook-Pro.local>
2015-12-08 21:07:50 +0000

    WIP

--- src/main/scala/com/barclays/adacore/model/Covariance.scala
+++ src/main/scala/com/barclays/adacore/model/Covariance.scala
@@ -10,28 +10,36 @@ import scalaz.Scalaz._
 
 case object Covariance {
 
-  def features(records: RDD[AnonymizedRecord]): RDD[(String, Vector)] = {
-    val entries = records.keyBy(r => (r.maskedCustomerId, ETL.businessID(r))).mapValues(value => 1).reduceByKey(_ + _)
+  def features(records: RDD[AnonymizedRecord])(implicit minKTx: Int = 5): (RDD[((String, String), Vector)], RDD[(Long, List[(String, String)])]) = {
+    val entries: RDD[((Long, (String, String)), (Int, Double))] =
+      records.keyBy(r => (r.maskedCustomerId, ETL.businessID(r)))
+      .mapValues(value => (1, value.amount))
+      .reduceByKey(_ |+| _)
+
     val businessIdx = entries.map(_._1._2).distinct.zipWithIndex.mapValues(_.toInt).collect().toMap
 
     val custIdx: Broadcast[Map[Long, Long]] = records.context.broadcast(entries.map(_._1._1).distinct().zipWithIndex().collect().toMap)
 
-    entries.keyBy(_._1._2).mapValues(v => List((v._1._1, v._2)))
-    .reduceByKey(_ |+| _)
-    .mapPartitions(part => {
-      val custIdxBV: Map[Long, Long] = custIdx.value
+    val history: RDD[(Long, List[(String, String)])] =
+      entries.map(e => e._1._1 -> List((e._1._2, e._2)))
+      .reduceByKey(_ |+| _)
+      .mapValues(v => v.filter(_._2._1 > minKTx).sortBy(el => -el._2._2).map(_._1))
 
-      part.map(e => {
-        val (bId, col) = e
-        val sv = Vectors.sparse(col.size, col.map(el => custIdxBV(el._1).toInt -> el._2.toDouble))
-        (bId, sv)
-      })
-    })
+    (entries.keyBy(_._1._2).mapValues(v => List((v._1._1, v._2)))
+     .reduceByKey(_ |+| _)
+     .mapPartitions(part => {
+       val custIdxBV: Map[Long, Long] = custIdx.value
+
+       part.map(e => {
+         val (bId, col) = e
+         val sv = Vectors.sparse(col.size, col.map(el => custIdxBV(el._1).toInt -> el._2._1.toDouble))
+         (bId, sv)
+       })
+     }), history)
   }
 
-  def toCovariance(features: RDD[(String, Vector)]) = {
+  def toCovariance(features: RDD[((String, String), Vector)]): (Matrix, Map[(String, String), Int]) = {
     val data = new RowMatrix(features.map(_._2))
-    data.computeCovariance()
+    (data.computeCovariance(), features.map(_._1).collect().zipWithIndex.toMap)
   }
-
 }
--- src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
+++ src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
@@ -1,23 +1,35 @@
 package com.barclays.adacore.model
 
-import com.barclays.adacore.{AnonymizedRecord, Recommender}
+import com.barclays.adacore.model.LanzaroteBest
+import com.barclays.adacore.{RecommenderTrainer, AnonymizedRecord, Recommender}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.Matrix
 
+import scala.collection.immutable.IndexedSeq
 import scalaz.Scalaz._
 
-trait RecommenderTrainer {
-  def train(data: RDD[AnonymizedRecord]): Recommender
-}
-
 case object LanzaroteCoach extends RecommenderTrainer {
-  override def train(data: RDD[AnonymizedRecord])(@transient sc: SparkContext, n: Int): Recommender =
-    LanzaroteBest(Covariance.features(data) |> Covariance.toCovariance)(sc, n)
+  override def train(data: RDD[AnonymizedRecord]): Recommender = {
+    val (features, history) = Covariance.features(data)
+    val (cov, idx) = features |> Covariance.toCovariance
+    LanzaroteBest(cov, idx, history)
+  }
+
 }
 
-case class LanzaroteBest(knowledge: Matrix)(@transient sc: SparkContext, n: Int) extends Recommender {
+case class LanzaroteBest(knowledge: Matrix, idx: Map[(String, String), Int], userHistory: RDD[(Long, List[(String, String)])])
+  extends Recommender {
 
-  // returns customerId -> List[(merchantName, merchantTown)]
-  def recommendations(data: RDD[Long]): RDD[(Long, List[(String, String)])] = ???
+  def getBestForId(rowId: Int): Int = {
+    (for {
+      j <- (0 to knowledge.numCols - 1)
+      v = knowledge.apply(rowId, j)
+    } yield (v, j))
+    .foldLeft((-1.0, -1))((acc, vi) => if (acc._1 < vi._1) vi else acc)._2
+  }
+
+  override def recommendations(customers: RDD[Long], n: Int): RDD[(Long, List[(String, String)])] =
+      customers.map(userHistory.lookup(_)).map(l => l.head.map(k => getBestForId(idx(k)))))
+
 }
