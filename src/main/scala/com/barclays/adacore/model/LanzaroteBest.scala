87be62d25c3e1a4c0a2c53c33185d61601f92331
Victor Paraschiv <victorparaschiv@Victors-MacBook-Pro.local>
2015-12-08 19:28:03 +0000

    created the trainer

--- src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
+++ src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
@@ -3,9 +3,21 @@ package com.barclays.adacore.model
 import com.barclays.adacore.{AnonymizedRecord, Recommender}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.Matrix
 
-case class LanzaroteBest(path: String)(@transient sc: SparkContext, n: Int) extends Recommender {
+import scalaz.Scalaz._
+
+trait RecommenderTrainer {
+  def train(data: RDD[AnonymizedRecord]): Recommender
+}
+
+case object LanzaroteCoach extends RecommenderTrainer {
+  override def train(data: RDD[AnonymizedRecord])(@transient sc: SparkContext, n: Int): Recommender =
+    LanzaroteBest(Covariance.features(data) |> Covariance.toCovariance)(sc, n)
+}
+
+case class LanzaroteBest(knowledge: Matrix)(@transient sc: SparkContext, n: Int) extends Recommender {
 
   // returns customerId -> List[(merchantName, merchantTown)]
-  override def recommendations(data: RDD[AnonymizedRecord]): RDD[(Long, List[(String, String)])] = {}
+  def recommendations(data: RDD[Long]): RDD[(Long, List[(String, String)])] = ???
 }
