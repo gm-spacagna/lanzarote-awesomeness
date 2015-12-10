ff28c4debd97c14307afe24c2fe1f97b191a0d5e
Victor Paraschiv <victorparaschiv@Victors-MacBook-Pro.local>
2015-12-10 00:52:57 +0000

    Exception during evaluation fixed.

--- src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
+++ src/main/scala/com/barclays/adacore/model/LanzaroteBest.scala
@@ -19,13 +19,20 @@ class LanzaroteCoach() extends RecommenderTrainer {
 case class LanzaroteBest(scores: RDD[((String, String), List[((String, String), Double)])],
                          history: RDD[(Long, List[(String, String)])]) extends Recommender {
 
-  override def recommendations(customers: RDD[Long], n: Int): RDD[(Long, List[(String, String)])] =
-    customers.map(c =>
-      c -> history.lookup(c) match {
-        case (_, res) if res.isEmpty =>
-          c -> scores.take(n).map(_._2.head._1).toList
+  override def recommendations(customers: RDD[Long], n: Int): RDD[(Long, List[(String, String)])] = {
+    val defaultRecommendations = scores.keys.take(n).toIterable
 
-        case (_, res :: Nil) if res.size == 1 =>
-          c -> res.flatMap(k => scores.lookup(k).head.take(n).map(_._1))
-      })
+    customers
+    .map(e => e -> None).cogroup(history)
+    .map(e => e._1 -> e._2._2.flatten)
+    .flatMap(v => {
+      if (v._2.nonEmpty)
+        v._2.map(b => b -> v._1)
+      else
+        defaultRecommendations.map(b => b -> v._1)
+    })
+    .cogroup(scores)
+    .map(s => s._2._1.head -> List(s._2._2.head.head._1))
+    .reduceByKey(_ |+| _)
+  }
 }
