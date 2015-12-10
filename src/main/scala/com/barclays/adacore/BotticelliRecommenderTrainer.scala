2a7b2bb993e361e5da2b15080cd209c001e34a8f
Spacagna, Gianmario : Personal & Corporate Banking <Gianmario.Spacagna@barclayscorp.com>
2015-12-10 17:54:54 +0000

    optimized scalability and memory efficiency

--- src/main/scala/com/barclays/adacore/BotticelliRecommenderTrainer.scala
+++ src/main/scala/com/barclays/adacore/BotticelliRecommenderTrainer.scala
@@ -1,12 +1,10 @@
 package com.barclays.adacore
 
 import com.barclays.adacore.utils.Pimps._
-import com.barclays.adacore.utils.{TopElements, VPTree}
+import com.barclays.adacore.utils.{Logger, TopElements, VPTree}
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
-
-import scalaz.Scalaz._
 
 case class BotticelliCustomerFeatures(dayOfTheWeekWallet: Map[Int, Double],
                                       postcodeSectorWallet: Map[String, Double],
@@ -16,48 +14,54 @@ case class BotticelliCustomerFeatures(dayOfTheWeekWallet: Map[Int, Double],
                                       genderPdf: Map[String, Double],
                                       maritalStatusIdPdf: Map[Option[Int], Double],
                                       occupationIdPdf: Map[Option[Int], Double])
-case class BotticelliRecommenderTrainer(@transient sc: SparkContext, maxNumBusinessesPerNeighbour: Int, k: Int) extends RecommenderTrainer {
+
+case class BotticelliRecommenderTrainer(@transient sc: SparkContext,
+                                        maxNumBusinessesPerNeighbour: Int,
+                                        maxPrecomputedRecommendations: Int,
+                                        k: Int) extends RecommenderTrainer {
   def train(data: RDD[AnonymizedRecord]): Recommender = {
 
     def wallet[T](data: RDD[AnonymizedRecord], func: (AnonymizedRecord => T)): RDD[(Long, Map[T, Double])] =
       data.map(record => (record.maskedCustomerId, func(record)) -> 1).reduceByKey(_ + _)
-      .map {
-        case ((customerId, t), count) => customerId -> Map(t -> count)
+      .groupBy {
+        case ((customerId, _), _) => customerId
       }
-      .reduceByKey(_ |+| _)
-      .mapValues(_.normalize)
-
-    val businessWallets: Broadcast[Map[Long, Map[(String, String), Double]]] =
-      sc.broadcast(
-        wallet(data, _.businessKey)
-        .mapValues(businessesScores =>
-          TopElements.topN(businessesScores.toList)(_._2, maxNumBusinessesPerNeighbour).toMap
-        ).collect().toMap
+      .mapValues(
+        _.map {
+          case ((_, t), count) => t -> count
+        }
+        .toMap.normalize.toList.toMap
       )
 
+    Logger().info("Computing the category wallets...")
     val categoryWallets: Broadcast[Map[Long, Map[String, Double]]] =
       sc.broadcast(wallet(data, _.merchantCategoryCode).collect().toMap)
 
+    Logger().info("Computing the dai of week wallets...")
     val dayOfTheWeekWallet: Broadcast[Map[Long, Map[Int, Double]]] =
       sc.broadcast(wallet(data, _.dayOfWeek).collect().toMap)
 
+    Logger().info("Computing the postcode wallets...")
     val postcodeSectorWallet: Broadcast[Map[Long, Map[String, Double]]] =
       sc.broadcast(wallet(data.filter(_.businessPostcode.nonEmpty), _.businessPostcode.get.dropRight(2)).collect().toMap)
 
+    Logger().info("Computing the final customer features...")
     val customerFeatures: Array[(Long, BotticelliCustomerFeatures)] =
       data.map(record => record.maskedCustomerId -> record.generalizedCategoricalGroup.group).distinct().map {
         case (customerId, categoricalGroup) =>
           customerId -> BotticelliCustomerFeatures(dayOfTheWeekWallet = dayOfTheWeekWallet.value(customerId),
-            postcodeSectorWallet = postcodeSectorWallet.value(customerId),
+            postcodeSectorWallet = postcodeSectorWallet.value.getOrElse(customerId, Map.empty[String, Double]),
             categoryWallet = categoryWallets.value(customerId),
-            onlineActivePdf = categoricalGroup.groupBy(_.onlineActive).mapValues(_.size).normalize,
-            acornTypeIdPdf = categoricalGroup.groupBy(_.acornTypeId).mapValues(_.size).normalize,
-            genderPdf = categoricalGroup.groupBy(_.gender).mapValues(_.size).normalize,
-            maritalStatusIdPdf = categoricalGroup.groupBy(_.maritalStatusId).mapValues(_.size).normalize,
-            occupationIdPdf = categoricalGroup.groupBy(_.occupationId).mapValues(_.size).normalize)
+            onlineActivePdf = categoricalGroup.groupBy(_.onlineActive).mapValues(_.size).normalize.toList.toMap,
+            acornTypeIdPdf = categoricalGroup.groupBy(_.acornTypeId).mapValues(_.size).normalize.toList.toMap,
+            genderPdf = categoricalGroup.groupBy(_.gender).mapValues(_.size).normalize.toList.toMap,
+            maritalStatusIdPdf = categoricalGroup.groupBy(_.maritalStatusId).mapValues(_.size).normalize.toList.toMap,
+            occupationIdPdf = categoricalGroup.groupBy(_.occupationId).mapValues(_.size).normalize.toList.toMap)
       }.collect()
 
-    val customerFeaturesBV = sc.broadcast(customerFeatures.toMap)
+    postcodeSectorWallet.destroy()
+    categoryWallets.destroy()
+    dayOfTheWeekWallet.destroy()
 
     val distanceFunction: (BotticelliCustomerFeatures, BotticelliCustomerFeatures) => Double = {
       case ((customer1: BotticelliCustomerFeatures), (customer2: BotticelliCustomerFeatures)) =>
@@ -79,24 +83,52 @@ case class BotticelliRecommenderTrainer(@transient sc: SparkContext, maxNumBusin
         1 - (num / (den1 * den2))
     }
 
+    Logger().info("Building the vptree with " + customerFeatures.length + " customers...")
     val vpTree = VPTree(customerFeatures.map(_.swap), distanceFunction, 1)
 
-    val vpTreeBV = sc.broadcast(vpTree)
+    Logger().info("Computing the business wallets...")
+    val businessWallets: Map[Long, Map[(String, String), Double]] =
+      wallet(data, _.businessKey)
+      .mapValues(businessesScores =>
+        TopElements.topN(businessesScores.toList)(_._2, maxNumBusinessesPerNeighbour).toMap
+      ).collect().toMap
 
+    Logger().info("Pre-computing recommendations...")
+    val preComputedRecommendationsBV = sc.broadcast(
+      customerFeatures.par.map {
+        case (customerId, features) => customerId -> {
+          val businessesScores =
+            (for {
+              (neighbourFeatures, neighbourId) <- vpTree.approximateNearestN(features, k).par
+              distance = distanceFunction(features, neighbourFeatures)
+              (neighbourBusiness, neighbourFrequency) <- businessWallets(neighbourId)
+              if !businessWallets(customerId).contains(neighbourBusiness)
+            } yield neighbourBusiness -> (neighbourFrequency * distance))
+            .groupBy(_._1).mapValues(_.map(_._2).sum).toList
+
+          TopElements.topN(businessesScores)(_._2, maxPrecomputedRecommendations).map(_._1)
+        }
+      }
+      .toList.toMap)
+
+    Logger().info("Computing the most popular businesses...")
+    val rankedBusinessesByNumberOfCustomersBV: Broadcast[List[(String, String)]] = sc.broadcast(
+      data.map(record => (record.businessKey, record.maskedCustomerId)).distinct()
+      .mapValues(_ => 1.0).reduceByKey(_ + _).sortBy(_._2, ascending = false).keys
+      .take(maxNumBusinessesPerNeighbour).toList
+    )
+
+    Logger().info("Creating the Recommender object...")
     new Recommender {
       // returns customerId -> List[(merchantName, merchantTown)]
       def recommendations(customers: RDD[Long], n: Int): RDD[(Long, List[(String, String)])] =
         customers.map(customerId => customerId -> {
-          val customerFeatures = customerFeaturesBV.value(customerId)
-          val recommendable =
-            (for {
-              (neighbourFeatures, neighbourId) <- vpTreeBV.value.approximateNearestN(customerFeatures, k).toList
-              distance = distanceFunction(customerFeatures, neighbourFeatures)
-              (neighbourBusiness, neighbourFrequency) <- businessWallets.value(neighbourId)
-            } yield neighbourBusiness -> (neighbourFrequency * distance))
-            .groupBy(_._1).mapValues(_.map(_._2).sum).toList
-
-          TopElements.topN(recommendable)(_._2, n).map(_._1)
+          val recommendations =
+            preComputedRecommendationsBV.value.getOrElse(customerId, rankedBusinessesByNumberOfCustomersBV.value)
+            .take(n)
+          if (recommendations.size >= n) recommendations
+          else (recommendations ++
+            (rankedBusinessesByNumberOfCustomersBV.value.take(n).toSet -- recommendations)).take(n)
         })
     }
   }
